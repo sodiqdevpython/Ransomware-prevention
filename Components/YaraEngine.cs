922601db1c9d88eb8ae935c56eb25129f4d30a1f
Sodiq Oydinov <sodiqdevpython@gmail.com>
2025-10-19 09:23:52 +0500

    Endi procces monitoringa yara bilan tekshirish qo'shildi har qanday ishga tushayotgan dasturni oldin ransomware ga tekshiraman rules.yar da ransomware uchun klasifikatsiya qilgan qoidalar bor

---
+++ Components/YaraEngine.cs
@@ -0,0 +1,81 @@
+﻿using Components;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using YaraClient;
+
+namespace ransomware.Components
+{
+    public class YaraEngine
+    {
+        public static void Run()
+        {
+
+            string rulesPath = @"C:\yara_rules\ransomware.yar";
+            string yaraDllDir = @"C:\yara_rules";
+            string[] wantedExts = { ".exe", ".dll", ".bat", ".cmd" };
+            string[] excludes = { @"\Windows\WinSxS", @"\node_modules" };
+            int cpuCount = 0;
+
+            var checker = new YaraChecker(rulesPath, wantedExts, excludes, dllDir: yaraDllDir, cpuCount: cpuCount);
+            var cts = new CancellationTokenSource();
+
+            ProcessWatcher.OnProcess += async (image, cmd, parentImage, pid, ppid) =>
+            {
+                if (parentImage != null && parentImage.Contains("explorer.exe"))
+                {
+                    try
+                    {
+                        var result = await checker.CheckFileAsync(image, cts.Token);
+
+                        if (result.Length > 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"[YARA rule ga tushdi] {image}");
+                            Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+                            Console.ResetColor();
+
+                            // Yara ga tushdi endi shu pid ni kill qildim
+                            Process process = Process.GetProcessById(pid);
+                            if(process != null)
+                            {
+                                process.Kill();
+                                Console.WriteLine($"Bu procces yopildi pid={pid}");
+                                Console.WriteLine($"Endi {pid} ochgan proccesslar qidirayabdi");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Bunday pid topilmadi={pid}");
+                            }
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"[OK] {image} - toza");
+                            Console.ResetColor();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"YARA tekshiruvda xatolik: {ex.Message}");
+                        Console.ResetColor();
+                    }
+                }
+            };
+
+
+            ProcessWatcher.Start();
+            Console.WriteLine("ProcessWatcher ishlamoqda...\n");
+
+            Console.ReadLine();
+
+            ProcessWatcher.Stop();
+            Console.WriteLine("ProcessWatcher to‘xtatildi.");
+
+            YaraScanManager.FinalizeYara();
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,11 @@
 ﻿using System;
+using Components;
+using ransomware.Components;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Components.ProcessWatcher.OnProcess += (image, cmd, parentImage, pid, ppid) =>
-        {
-            Console.WriteLine(
-                $"Image: {image}\nCommandLine: {cmd}\nParentImage: {parentImage}\npid={pid}, ppid={ppid}\n"
-            );
-        };
-
-        Components.ProcessWatcher.Start();
-
-        Console.WriteLine("ProcessWatcher ishlamoqda... Enter bosilsa to‘xtaydi.");
-        Console.ReadLine();
-
-        Components.ProcessWatcher.Stop();
+        YaraEngine.Run();
     }
 }
